afee03a922a75237f3cf496f25de9c0ce4b4cfb6
asupan <antonio.suups@gmail.com>
2021-06-02 08:59:51 +0200

    loading spinner, register komponenta temelj

---
+++ ApiGateway/AutoStoper.Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoStoper.Client.Pages.Authentication
+{
+    public partial class Register
+    {
+    }
+}
